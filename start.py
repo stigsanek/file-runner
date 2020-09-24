fc73a9708338fd87c7577cd30166cbddfc0b3402
stigsanek <stigsanek@yandex.ru>
2020-09-25 00:07:40 +0400

    Добавляет отдельный файл для задания конфигураций входного и выходного пути

--- start.py
+++ start.py
@@ -1,11 +1,14 @@
 import os, shutil
 
-PATH = {
-    'input': 'input',
-    'output': 'output'
-}
-
+path_dir = {'input': '', 'output': ''}
 errors = []
+
+def define_path():
+    with open('path.txt', 'r') as path_config:
+        line = path_config.read()
+        configs = line.split(',')
+        path_dir['input'] = configs[0].strip()
+        path_dir['output'] = configs[1].strip()
 
 def logs_error():
     with open('error.txt', 'w') as error_file:
@@ -16,12 +19,13 @@ def start_copying():
     with open('files.txt', 'r') as files:
         for line in files:
             file_name = line.strip()
-            file_path = os.path.join(PATH['input'], file_name)
+            file_path = os.path.join(path_dir['input'], file_name)
 
             try:
-                shutil.copy(file_path, PATH['output'])
+                shutil.copy(file_path, path_dir['output'])
             except:
                 errors.append(file_name)
 
+define_path()
 start_copying()
 logs_error()
